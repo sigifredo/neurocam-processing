1730fb2e269bdd3da7abe3f0a31338042e2c4aba
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2024-11-17 19:30:12 -0500

    Se implementa la clase Range.

--- Range.java
+++ Range.java
@@ -1,11 +1,41 @@
 
 public class Range {
 
-    public float start;
-    public float end;
+    private float start;
+    private float end;
 
     public Range() {
         this.start = 0.0f;
         this.end = 0.0f;
+    }
+
+    public void expand(float number) {
+        if (number < this.start) {
+            this.start = number;
+        } else if (number > this.end) {
+            this.end = number;
+        }
+
+        if (this.start > this.end) {
+            this.start += this.end;
+            this.end = this.start - this.end;
+            this.start -= this.end;
+        }
+    }
+
+    public float getStart() {
+        return this.start;
+    }
+
+    public float getEnd() {
+        return this.end;
+    }
+
+    public static float map(float n, Range in, Range out) {
+        return map(n, in.start, in.end, out.start, out.end);
+    }
+
+    public static float map(float n, float inStart, float inEnd, float outStart, float outEnd) {
+        return outStart + (n - inStart) * (outEnd - outStart) / (inEnd - inStart);
     }
 }
